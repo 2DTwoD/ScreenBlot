e8edab1711ebd509a9ca1a4f5881aa0616d036bc
Dmitry <113849847+2DTwoD@users.noreply.github.com>
2026-03-24 16:28:48 +0500

    development

--- mouse_coordinate.py
+++ mouse_coordinate.py
@@ -3,20 +3,19 @@ import pynput
 
 class MouseCoordinate:
     def __init__(self, root_canvas):
-        self.pressed = False
         self.root_canvas = root_canvas
         mouse_listener = pynput.mouse.Listener(on_move=self.on_move, on_click=self.on_click)
         mouse_listener.start()
 
     def on_move(self, x, y):
-        if self.pressed:
-            self.root_canvas.new_coordinate(x, y)
+        self.root_canvas.global_mouse_move_action(x, y)
 
     def on_click(self, x, y, button, pressed):
         if button == pynput.mouse.Button.left:
-            self.pressed = pressed
             if pressed:
-                self.root_canvas.start_drawing(x, y)
+                self.root_canvas.global_lmouse_down_action(x, y)
             else:
-                self.root_canvas.end_drawing()
+                self.root_canvas.global_lmouse_up_action(x, y)
+        elif button == pynput.mouse.Button.right:
+            self.root_canvas.global_rmouse_down_action(x, y)
 
--- root_canvas.py
+++ root_canvas.py
@@ -8,6 +8,8 @@ class ShapeType(Enum):
     RECTANGLE = 1,
     OVAL = 2,
     POLYGON = 3,
+    TEXT = 4,
+
 
 class RootCanvas(tk.Canvas):
     def __init__(self, root, transparent_color):
@@ -20,7 +22,10 @@ class RootCanvas(tk.Canvas):
         clear.pack(anchor=tk.NE)
         undo.pack(anchor=tk.NE)
 
-        self.type = ShapeType.POLYGON
+        self.type = ShapeType.TEXT
+
+        self.mouse_down = False
+        self.start_click_drawing = False
 
         self.coordinates = []
 
@@ -30,38 +35,50 @@ class RootCanvas(tk.Canvas):
         self.move_shift_x = 0
         self.move_shift_y = 0
 
-    def get_start_coords(self):
-        if len(self.coordinates) > 0:
-            return self.coordinates[0]
-
-    def get_coord_increment(self):
-        if len(self.coordinates) > 1:
-            return map(operator.sub, self.coordinates[-1], self.coordinates[-2])
-
-    def start_drawing(self, x, y):
+    def global_lmouse_down_action(self, x, y):
+        self.mouse_down = True
+        self.start_click_drawing = self.type == ShapeType.POLYGON
         self.coordinates.append((x, y))
 
-    def new_coordinate(self, x, y):
-        self.coordinates.append((x, y))
-        if self.shape_for_moving is None:
-            self.after(0, lambda: self.draw_shape())
-        else:
-            self.move(self.shape_for_moving, *self.get_coord_increment())
+    def global_mouse_move_action(self, x, y):
+        if self.mouse_down or self.start_click_drawing:
+            self.coordinates.append((x, y))
+            if self.shape_for_moving is None:
+                self.after(0, lambda: self.draw_shape())
+            else:
+                self.move(self.shape_for_moving, *self.get_coords_increment())
+
+    def global_lmouse_up_action(self, x, y):
+        self.mouse_down = False
+        if self.type == ShapeType.POLYGON:
+            self.shape_for_moving = None
+            return
+        self.end_drawing()
+
+    def global_rmouse_down_action(self, x, y):
+        if self.start_click_drawing:
+            self.after(1, self.end_drawing)
+            self.coordinates.clear()
+            self.start_click_drawing = False
 
     def draw_shape(self):
         if self.shape_id is not None:
             self.delete(self.shape_id)
         match self.type:
             case ShapeType.LINE:
+                self.shape_id = self.create_oval(self.coordinates, width=3, outline='white')
                 self.shape_id = self.create_line(self.coordinates, width=3, fill='white')
             case ShapeType.RECTANGLE:
                 self.shape_id = self.create_rectangle(self.coordinates, width=3, outline='white')
                 self.coordinates.pop(-1)
             case ShapeType.OVAL:
-                self.shape_id = self.create_oval(self.coordinates, width=3, outline='white')
                 self.coordinates.pop(-1)
             case ShapeType.POLYGON:
                 self.shape_id = self.create_polygon(self.coordinates, width=3, outline='white', fill='red')
+                self.coordinates.pop(-1)
+            case ShapeType.TEXT:
+                self.shape_id = self.create_text(self.get_start_coords(), fill="red", text="Test")
+                self.coordinates.pop(-1)
 
     def end_drawing(self):
         self.coordinates.clear()
@@ -69,12 +86,13 @@ class RootCanvas(tk.Canvas):
         if self.shape_id is None:
             return
         tmp_shape_id = self.shape_id
-        self.tag_bind(self.shape_id, '<Button-1>',lambda e: self.shape_click_action(tmp_shape_id))
-        self.tag_bind(self.shape_id, '<Button-3>',lambda e: self.delete(tmp_shape_id))
+        self.tag_bind(self.shape_id, '<Button-1>', lambda e: self.shape_click_action(tmp_shape_id))
+        self.tag_bind(self.shape_id, '<Button-3>', lambda e: self.delete(tmp_shape_id))
         self.shape_id = None
 
     def shape_click_action(self, shape_id):
         self.shape_for_moving = shape_id
+        self.start_click_drawing = False
 
     def undo_action(self):
         if len(self.find_all()) > 0:
@@ -83,3 +101,11 @@ class RootCanvas(tk.Canvas):
     def clear_action(self):
         for shape in self.find_all():
             self.delete(shape)
+
+    def get_start_coords(self):
+        if len(self.coordinates) > 0:
+            return self.coordinates[0]
+
+    def get_coords_increment(self):
+        if len(self.coordinates) > 1:
+            return map(operator.sub, self.coordinates[-1], self.coordinates[-2])
